ad783d06c1293c80a8f8c07901e0242ac5a17116
Felix Bueppelmann <fbueppelmann@gmail.com>
2021-11-09 16:48:00 +0100

    Add custom error message

--- he46pusa/function.py
+++ he46pusa/function.py
@@ -3,7 +3,11 @@ from ipywidgets import interact, fixed
 from PIL import Image
 
 def imshow(X, resize=None):
-    row = X.shape[0]  
+    row = X.shape[0]
     col = X.shape[1]
-    return [[ X[int(row * r / resize[0])][int(col * c / resize[1])]
-             for c in range(resize[1])] for r in range(resize[0])]
+    try:
+        resized_image = [[ X[int(row * r / resize[0])][int(col * c / resize[1])]
+                            for c in range(resize[1])] for r in range(resize[0])]
+    except:
+        raise Exception('The resize input must be a list or tuple')
+    return resized_image
