9dc7a2432dc6c8e4c54de777ef445c1657e6d8bb
Felix Bueppelmann <fbueppelmann@gmail.com>
2021-11-09 16:38:18 +0100

    Add image resize function

--- he46pusa/function.py
+++ he46pusa/function.py
@@ -3,12 +3,7 @@ from ipywidgets import interact, fixed
 from PIL import Image
 
 def imshow(X, resize=None):
-    pass
-
-
- """
- You should create a way to resize an image from an array X.
- The use of widgets is optional but you can take a look to interact.
- We should be able to install this package in Google Colab from your Git
-repo.
- """
+    row = X.shape[0]  
+    col = X.shape[1]
+    return [[ X[int(row * r / resize[0])][int(col * c / resize[1])]
+             for c in range(resize[1])] for r in range(resize[0])]
