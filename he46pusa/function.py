665338a17c58e2470ffe64df6c22ecce96b3116b
Felix Bueppelmann <fbueppelmann@gmail.com>
2021-11-11 12:10:23 +0100

    Changed function to use Pillow

--- he46pusa/function.py
+++ he46pusa/function.py
@@ -3,11 +3,9 @@ from ipywidgets import interact, fixed
 from PIL import Image
 
 def imshow(X, resize=None):
-    row = X.shape[0]
-    col = X.shape[1]
+    X_new = Image.fromarray(X)
     try:
-        resized_image = [[ X[int(row * r / resize[0])][int(col * c / resize[1])]
-                            for c in range(resize[1])] for r in range(resize[0])]
+        X_new = X_new.resize(resize)
     except:
         raise Exception('The resize input must be a list or tuple')
-    return resized_image
+    return X_new
